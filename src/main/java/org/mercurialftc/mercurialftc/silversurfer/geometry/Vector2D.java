9764beead220ddeeb76be4ca49737968b0a7db83
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-09-11 15:42:45 +1000

    fixed scalar multiplication of vectors

--- src/main/java/org/mercurialftc/mercurialftc/silversurfer/geometry/Vector2D.java
+++ src/main/java/org/mercurialftc/mercurialftc/silversurfer/geometry/Vector2D.java
@@ -2,110 +2,12 @@ package org.mercurialftc.mercurialftc.silversurfer.geometry;
 
 public class Vector2D {
 	private double x, y;
-	
-	public double getX() {
-		return x;
-	}
-	
-	public void setX(double x) {
-		this.x = x;
-	}
-	
-	public double getY() {
-		return y;
-	}
-	
-	public void setY(double y) {
-		this.y = y;
-	}
-	
+
 	public Vector2D(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
-	public Angle getHeading() {
-		return new AngleRadians(Math.atan2(y, x));
-	}
-	
-	public double getMagnitude() {
-		return Math.hypot(x, y);
-	}
-	
-	public Vector2D set(double x, double y) {
-		this.x = x;
-		this.y = y;
-		return this;
-	}
-	
-	/**
-	 * non-mutating
-	 * @param x
-	 * @param y
-	 * @return a new vector with the desired operation applied
-	 */
-	public Vector2D add(double x, double y) {
-		return new Vector2D(this.x + x, this.y + y);
-	}
-	
-	/**
-	 * non-mutating
-	 * @param x
-	 * @param y
-	 * @return a new vector with the desired operation applied
-	 */
-	public Vector2D subtract(double x, double y) {
-		return new Vector2D(this.x - x, this.y - y);
-		
-	}
-	
-	/**
-	 * non-mutating
-	 * @param other
-	 * @return a new vector with the desired operation applied
-	 */
-	public Vector2D add(Vector2D other) {
-		return this.add(other.x, other.y);
-	}
-	
-	/**
-	 * non-mutating
-	 * @param other
-	 * @return a new vector with the desired operation applied
-	 */
-	public Vector2D subtract(Vector2D other) {
-		return this.subtract(other.x, other.y);
-	}
-	
-	/**
-	 * non-mutating
-	 * @param factor the scalar multiplication
-	 * @return a new vector with the desired operation applied
-	 */
-	public Vector2D scalarMultiply(double factor) {
-		return new Vector2D(factor * this.getX(), factor * this.getY());
-	}
 
-	/**
-	 * dot product
-	 * @param other
-	 * @return
-	 */
-	public double dot(Vector2D other) {
-		return this.getX() * other.getX() + this.getY() * other.getY();
-	}
-
-	/**
-	 * rotates anti-clockwise
-	 * @param angle
-	 * @return
-	 */
-	public Vector2D rotate(Angle angle) {
-		double cos = Math.cos(angle.getRadians());
-		double sin = Math.sin(angle.getRadians());
-		return new Vector2D(cos * getX() - sin * getY(), sin * getX() + cos * getY());
-	}
-	
 	/**
 	 * The polar constructor for vectors, uses radians
 	 *
@@ -116,7 +18,7 @@ public class Vector2D {
 	public static Vector2D fromPolar(double r, double t) {
 		return fromPolar(r, new AngleRadians(t));
 	}
-	
+
 	/**
 	 * The polar constructor for vectors
 	 *
@@ -126,5 +28,110 @@ public class Vector2D {
 	 */
 	public static Vector2D fromPolar(double r, Angle t) {
 		return new Vector2D(r * Math.cos(t.getRadians()), r * Math.sin(t.getRadians()));
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public Angle getHeading() {
+		return new AngleRadians(Math.atan2(y, x));
+	}
+
+	public double getMagnitude() {
+		return Math.hypot(x, y);
+	}
+
+	public Vector2D set(double x, double y) {
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+
+	/**
+	 * non-mutating
+	 *
+	 * @param x
+	 * @param y
+	 * @return a new vector with the desired operation applied
+	 */
+	public Vector2D add(double x, double y) {
+		return new Vector2D(this.x + x, this.y + y);
+	}
+
+	/**
+	 * non-mutating
+	 *
+	 * @param x
+	 * @param y
+	 * @return a new vector with the desired operation applied
+	 */
+	public Vector2D subtract(double x, double y) {
+		return new Vector2D(this.x - x, this.y - y);
+
+	}
+
+	/**
+	 * non-mutating
+	 *
+	 * @param other
+	 * @return a new vector with the desired operation applied
+	 */
+	public Vector2D add(Vector2D other) {
+		return this.add(other.x, other.y);
+	}
+
+	/**
+	 * non-mutating
+	 *
+	 * @param other
+	 * @return a new vector with the desired operation applied
+	 */
+	public Vector2D subtract(Vector2D other) {
+		return this.subtract(other.x, other.y);
+	}
+
+	/**
+	 * non-mutating
+	 *
+	 * @param factor the scalar multiplication
+	 * @return a new vector with the desired operation applied
+	 */
+	public Vector2D scalarMultiply(double factor) {
+		return fromPolar(getMagnitude() * factor, getHeading());
+	}
+
+	/**
+	 * dot product
+	 *
+	 * @param other
+	 * @return
+	 */
+	public double dot(Vector2D other) {
+		return this.getX() * other.getX() + this.getY() * other.getY();
+	}
+
+	/**
+	 * rotates anti-clockwise
+	 *
+	 * @param angle
+	 * @return
+	 */
+	public Vector2D rotate(Angle angle) {
+		double cos = Math.cos(angle.getRadians());
+		double sin = Math.sin(angle.getRadians());
+		return new Vector2D(cos * getX() - sin * getY(), sin * getX() + cos * getY());
 	}
 }

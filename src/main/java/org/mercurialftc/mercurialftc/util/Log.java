070b8149899b89994de249b8e5a8ca59b77fe524
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-09-19 09:35:12 +1000

    improved docs and changed to use maps for data headings, for more telemetry like behaviour

--- src/main/java/org/mercurialftc/mercurialftc/util/Log.java
+++ src/main/java/org/mercurialftc/mercurialftc/util/Log.java
@@ -1,46 +1,45 @@
 package org.mercurialftc.mercurialftc.util;
 
 import android.os.Environment;
+import org.jetbrains.annotations.NotNull;
 import org.mercurialftc.mercurialftc.scheduler.Scheduler;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 
 public class Log {
 	private final long startTime;
 	private final String now;
 
 	private final String[] dataLine;
-	private final String[] dataHeadings;
+
+	private final LinkedHashMap<String, Integer> dataHeadings;
 	private FileWriter fileWriter;
 
-	public Log(String subsystem, int storedLogs, String... dataHeadings) {
+	public Log(String system, int storedLogs, @NotNull String... dataHeadings) {
 		this.startTime = System.nanoTime();
 		this.now = String.valueOf(System.nanoTime() / 1E9);
-		this.dataHeadings = new String[dataHeadings.length + 1];
-		this.dataLine = new String[this.dataHeadings.length];
-		System.arraycopy(dataHeadings, 0, this.dataHeadings, 1, dataHeadings.length);
+		this.dataHeadings = new LinkedHashMap<>();
+		this.dataHeadings.put("ElapsedTime", 0);
+		int i = 1;
+		for (String heading :
+				dataHeadings) {
+			this.dataHeadings.put(heading, i);
+			i++;
+		}
+		this.dataLine = new String[dataHeadings.length + 1];
 
 		if (!Scheduler.isLoggingEnabled()) {
 			return;
 		}
 
-		String directoryPath = Environment.getExternalStorageDirectory().getPath() + "/FIRST/mercurialftc/logs/" + subsystem;
-		File directory = new File(directoryPath);
-		directory.mkdirs();
-
-		File outdatedLog = new File(directoryPath + "/" + subsystem + (storedLogs - 1) + ".csv");
-		outdatedLog.delete();
-		for (int i = storedLogs; i > 0; i--) {
-			File oldLog = new File(directoryPath + "/" + subsystem + (i - 1) + ".csv");
-			File oldLogDestination = new File(directoryPath + "/" + subsystem + (i) + ".csv");
-			oldLog.renameTo(oldLogDestination);
-		}
+		String directoryPath = clearToDirectoryPath(system, storedLogs);
 		try {
-			fileWriter = new FileWriter(directoryPath + "/" + subsystem + "0.csv", true);
+			fileWriter = new FileWriter(directoryPath + "/" + system + "0.csv", true);
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 
 		createHeadings();
@@ -48,6 +47,29 @@ public class Log {
 
 	public Log(String subsystem, String... dataHeadings) {
 		this(subsystem, 6, dataHeadings);
+	}
+
+	/**
+	 * removes oldest log and shifts all the others down one
+	 *
+	 * @param system     name of the logFIle
+	 * @param storedLogs number of logs to be stored
+	 * @return the path to the log folder
+	 */
+	@NotNull
+	private static String clearToDirectoryPath(String system, int storedLogs) {
+		String directoryPath = Environment.getExternalStorageDirectory().getPath() + "/FIRST/mercurialftc/logs/" + system;
+		File directory = new File(directoryPath);
+		directory.mkdirs();
+
+		File outdatedLog = new File(directoryPath + "/" + system + (storedLogs - 1) + ".csv");
+		outdatedLog.delete();
+		for (int i = storedLogs; i > 0; i--) {
+			File oldLog = new File(directoryPath + "/" + system + (i - 1) + ".csv");
+			File oldLogDestination = new File(directoryPath + "/" + system + (i) + ".csv");
+			oldLog.renameTo(oldLogDestination);
+		}
+		return directoryPath;
 	}
 
 	public void updateLoop(boolean storeTime) {
@@ -69,15 +91,21 @@ public class Log {
 		try {
 			fileWriter.write(dataWrite + "\n");
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 
-	public void logData(int dataHeadingArrayPosition, Object data) {
+	/**
+	 * @param dataHeading a previously set data heading
+	 * @param data        the data object to store, Object.toString is automatically called on it
+	 */
+	public void logData(String dataHeading, Object data) {
 		if (!Scheduler.isLoggingEnabled()) {
 			return;
 		}
-		dataLine[dataHeadingArrayPosition + 1] = data.toString();
+		Integer dataHeadingArrayPosition = dataHeadings.get(dataHeading);
+		if (dataHeadingArrayPosition == null) return;
+		dataLine[dataHeadingArrayPosition] = data.toString();
 	}
 
 	public void close() {
@@ -87,7 +115,7 @@ public class Log {
 		try {
 			fileWriter.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 
@@ -98,10 +126,14 @@ public class Log {
 		try {
 			fileWriter.write(now + "\n");
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		this.dataHeadings[0] = "Elapsed Time";
-		System.arraycopy(dataHeadings, 0, dataLine, 0, dataHeadings.length);
+		int i = 0;
+		for (String heading : dataHeadings.keySet()) {
+			dataLine[i] = heading;
+			i++;
+		}
+
 		updateLoop(false);
 	}
 }

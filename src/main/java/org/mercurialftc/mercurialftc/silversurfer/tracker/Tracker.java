59eb672327e5c6854a256bd39d5c6fdef0427c6a
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-09-28 13:46:35 +1000

    suppressed unused warnings

--- src/main/java/org/mercurialftc/mercurialftc/silversurfer/tracker/Tracker.java
+++ src/main/java/org/mercurialftc/mercurialftc/silversurfer/tracker/Tracker.java
@@ -55,6 +55,7 @@ public abstract class Tracker {
 	/**
 	 * must be called frequently for it to be accurate
 	 */
+	@SuppressWarnings("unused")
 	public void updatePose() {
 		previousPose2D = pose2D;
 		updateValues();

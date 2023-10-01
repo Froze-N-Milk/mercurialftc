ef4bd486485f3f1fa0a70007b4b073cec31a3a94
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-10-01 11:29:20 +1100

    rebuilt GVF WaveFollower, still to be tested

---
+++ src/main/java/org/mercurialftc/mercurialftc/silversurfer/follower/GVFWaveFollower.java
@@ -0,0 +1,64 @@
+package org.mercurialftc.mercurialftc.silversurfer.follower;
+
+import org.jetbrains.annotations.NotNull;
+import org.mercurialftc.mercurialftc.silversurfer.followable.Followable;
+import org.mercurialftc.mercurialftc.silversurfer.followable.motionconstants.MecanumMotionConstants;
+import org.mercurialftc.mercurialftc.silversurfer.geometry.Pose2D;
+import org.mercurialftc.mercurialftc.silversurfer.geometry.Vector2D;
+import org.mercurialftc.mercurialftc.silversurfer.tracker.Tracker;
+
+public class GVFWaveFollower extends WaveFollower {
+	private final ArbFollower arbFollower;
+	private final Tracker tracker;
+
+	public GVFWaveFollower(@NotNull ArbFollower arbFollower, Tracker tracker) {
+		super(arbFollower.getMotionConstants());
+		this.arbFollower = arbFollower;
+		this.tracker = tracker;
+	}
+
+	@Override
+	protected void followOutput(@NotNull Followable.Output output, double loopTime) {
+		Pose2D velocityPose = tracker.getPose2D().subtract(tracker.getPreviousPose2D());
+		Vector2D velocityVector = new Vector2D(velocityPose.getX(), velocityPose.getY()).scalarMultiply(1 / loopTime);
+
+		Pose2D errorPose = output.getPosition().subtract(tracker.getPose2D());
+		Vector2D errorVector = new Vector2D(errorPose.getX(), errorPose.getY());
+
+		Vector2D transformedTranslationVector = output.getTranslationVector();
+		if (errorVector.getMagnitude() > 3) {
+			transformedTranslationVector = transformedTranslationVector.add(errorVector);
+		}
+
+		MecanumMotionConstants.DirectionOfTravelLimiter directionOfTravelLimiter = arbFollower.getMotionConstants().makeDirectionOfTravelLimiter(transformedTranslationVector.getHeading());
+
+		double targetVelocity = transformedTranslationVector.getMagnitude();
+		double maxVelocity = velocityVector.getMagnitude() + loopTime * directionOfTravelLimiter.getAcceleration();
+		double minVelocity = velocityVector.getMagnitude() - loopTime * directionOfTravelLimiter.getAcceleration();
+
+		transformedTranslationVector = Vector2D.fromPolar(Math.max(minVelocity, Math.min(targetVelocity, maxVelocity)), transformedTranslationVector.getHeading());
+
+		double transformedRotationalVelocity = output.getRotationalVelocity();
+		double rotationalError = tracker.getPose2D().getTheta().findShortestDistance(output.getPosition().getTheta());
+		double acceptableError = tracker.getPreviousPose2D().getTheta().findShortestDistance(tracker.getPose2D().getTheta());
+		double rotationalVelocity = acceptableError / loopTime;
+
+		if (Math.abs(rotationalError) > Math.abs(acceptableError)) {
+			transformedRotationalVelocity += rotationalError;
+		}
+
+		double maxRotationalVelocity = rotationalVelocity + loopTime * getMotionConstants().getMaxRotationalAcceleration();
+		double minRotationalVelocity = rotationalVelocity - loopTime * getMotionConstants().getMaxRotationalAcceleration();
+
+		transformedRotationalVelocity = Math.max(minRotationalVelocity, Math.min(transformedRotationalVelocity, maxRotationalVelocity));
+
+		Followable.Output tranformedOutput = new Followable.Output(
+				transformedTranslationVector,
+				transformedRotationalVelocity,
+				output.getCallbackTime(),
+				output.getPosition(),
+				output.getDestination()
+		);
+		arbFollower.followOutput(tranformedOutput, loopTime);
+	}
+}

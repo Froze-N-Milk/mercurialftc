d1efe287cba70d8d863c9b4a22710fc307dd9aca
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-10-11 15:52:43 +1100

    added default pose wrapper

--- src/main/java/org/mercurialftc/mercurialftc/silversurfer/geometry/obstaclemap/obstacle/Obstacle.java
+++ src/main/java/org/mercurialftc/mercurialftc/silversurfer/geometry/obstaclemap/obstacle/Obstacle.java
@@ -1,8 +1,22 @@
 package org.mercurialftc.mercurialftc.silversurfer.geometry.obstaclemap.obstacle;
 
+import org.jetbrains.annotations.NotNull;
+import org.mercurialftc.mercurialftc.silversurfer.geometry.Pose2D;
 import org.mercurialftc.mercurialftc.silversurfer.geometry.Vector2D;
 
 @SuppressWarnings("unused")
 public interface Obstacle {
+	/**
+	 * @param position position of the robot
+	 * @return the distance from the robot to the closest point of the obstacle
+	 */
 	Vector2D distance(Vector2D position);
+
+	/**
+	 * @param position position of the robot
+	 * @return the distance from the robot to the closest point of the obstacle
+	 */
+	default Vector2D distance(@NotNull Pose2D position) {
+		return distance(position.toVector2D());
+	}
 }

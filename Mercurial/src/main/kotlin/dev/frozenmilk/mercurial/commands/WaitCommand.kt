0925b684e6cecaefa12171865f216cfa39e188e7
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-12-20 07:41:55 +1100

    added wait command, which is a quick way to do a timed delay, or can be extended through to toLambda() function to perform something for an amount of time

--- Mercurial/src/main/kotlin/dev/frozenmilk/mercurial/commands/WaitCommand.kt
+++ Mercurial/src/main/kotlin/dev/frozenmilk/mercurial/commands/WaitCommand.kt
@@ -1,4 +1,28 @@
 package dev.frozenmilk.mercurial.commands
 
-class WaitCommand {
+import dev.frozenmilk.mercurial.RunStates
+import dev.frozenmilk.mercurial.subsystems.Subsystem
+
+/**
+ * a command that waits for the specified duration, in seconds
+ */
+class WaitCommand(val duration: Double) : Command {
+	var startTime = 0L
+		private set
+	override fun initialise() {
+		startTime = System.nanoTime()
+	}
+
+	override fun execute() {
+	}
+
+	override fun end(interrupted: Boolean) {
+	}
+
+	override fun finished(): Boolean {
+		return (System.nanoTime() - startTime) / 1E9 >= duration
+	}
+
+	override val requiredSubsystems: Set<Subsystem> = emptySet()
+	override val runStates: Set<RunStates> = setOf(RunStates.INIT_LOOP, RunStates.LOOP)
 }
